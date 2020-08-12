b4724c4f99efc04a6f39ccd03aaadb7420a555e9
dannynurdin <dannyfachrul@gmail.com>
2020-08-12 16:14:33 +0700

    kill bug in purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 './src/**/*.vue',
                 './public/index.html',
             ],
-            defaultExtractorKey: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
         })
     ]
 }
